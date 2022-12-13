20172c95c6177b6d2d5448a0bb5c4ef37a2fdee3
Buga Karina <memnaya@mail.ru>
2022-12-13 10:23:23 +0300

    add games

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/named
+import readlineSync from 'readline-sync';
+// eslint-disable-next-line import/named
+import {
+  greeting, randomNum, getRandomString, getRandomExepcion,
+} from '../index.js';
+
+const calculate = () => {
+  const nameUser = greeting();
+
+  /* console.log(correctAnswer); */
+
+  console.log('What is the result of the expression?');
+  let i = 0;
+  while (i < 3) {
+    const randomNum1 = randomNum();
+    const randomNum2 = randomNum();
+    const numsStr = getRandomString(randomNum1, randomNum2);
+
+    console.log(`Question: ${numsStr}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = getRandomExepcion(numsStr);
+    if (Number(answer) === correctAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${nameUser}!`);
+      i -= i;
+    }
+  }
+  return console.log(`Congratulations, ${nameUser}!`);
+};
+
+export default calculate;
