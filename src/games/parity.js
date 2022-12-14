6dce44a0eaacd5897e8548da3578cec8c0986499
Buga Karina <memnaya@mail.ru>
2022-12-14 20:46:07 +0300

    fix mistake

--- src/games/parity.js
+++ src/games/parity.js
@@ -20,6 +20,9 @@ const parityGame = () => {
     if ((randomNum % 2 === 0 && answer === 'yes') || (randomNum % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
       i += 1;
+    } else if (randomNum === 0 && answer === 'yes') {
+      console.log('Correct!');
+      i += 1;
     } else if (randomNum % 2 !== 0 && answer === 'yes') {
       console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
       console.log(`Let's try again, ${name}!`);
